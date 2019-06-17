de9fa991f0a3cc3cf804fc6e387c8ec8fc90f4a1
Adam Sultanov <adam.sultanov@gmail.com>
2019-06-17 11:21:09 -0400

    little commit

---
+++ src/index.js
@@ -0,0 +1,46 @@
+window.addEventListener("DOMContentLoaded", init)
+
+function init(){
+  console.log("%c エッグモン", "font-size:24px; font-weight:bold; color:red; text-shadow:2px 2px 2px orange")
+
+  /*
+      STATE 1
+      - sign in / sign up
+      - splash graphic
+      - random other pets
+
+      STATE 2
+      - sign out
+      - pet interaction area
+      - random other pets
+  */
+
+
+
+
+  // initialize page with pets
+    // get user
+    // get pet data
+    // render pet data to html
+    // append to dom
+
+  // listen for clicks on:
+    // sign up/sign in
+    // create new pet
+    // "edit" pet (feed, play, etc)
+    // delete pet
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
